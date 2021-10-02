af412c7ca06708a30f7db0f28ec7ae83334918c4
Brooke Hellerich <brookeh815@cox.net>
2021-10-02 12:27:30 -0500

    commit tree 9ece5490a626607692768756c658b68b30240004

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+//const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+//const firstTwo = drivers.slice(0,2);
+//const lastTwo = drivers.slice(-2);
+
+// function returnFirstTwoDrivers() {
+//     return firstTwo;
+// }
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier (integer) {
+    return function () {
+        return integer * integer
+    }
+}
+
+let fareDoubler = fare => fare * 2
+let fareTripler = fare => fare * 3
+
+function selectDifferentDrivers(drivers, callback) {
+    return callback(drivers);
+}
